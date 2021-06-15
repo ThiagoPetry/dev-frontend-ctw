23fde3a5de177905513c11bb0ccc146826131f17
Thiago Keiser Petry <thiagokeiserpetry@gmail.com>
2021-06-15 07:37:06 -0300

    JS fechar menu direito

---
+++ Dashboard/js/close-menu-right.js
@@ -0,0 +1,65 @@
+document.getElementById("cont1").addEventListener('mouseout', close1);
+
+function close1() {
+    document.getElementById("cont1").style.transform = 'translateX(-0vh)';
+    document.getElementById("img1").style.transform = 'scale(1, 1)';
+    document.getElementById("img1").style.marginLeft = '0.25vw';
+    document.getElementById("cont1").classList.remove("openMenuRight");
+    document.getElementById("img1").classList.remove("appearMenuRight");
+    document.getElementById("txt1").classList.remove("appearMenuRight");
+}
+
+document.getElementById("cont2").addEventListener('mouseout', close2);
+
+function close2() {
+    document.getElementById("cont2").style.transform = 'translateX(-0vh)';
+    document.getElementById("img2").style.transform = 'scale(1, 1)';
+    document.getElementById("img2").style.marginLeft = '0.25vw';
+    document.getElementById("cont2").classList.remove("openMenuRight");
+    document.getElementById("img2").classList.remove("appearMenuRight");
+    document.getElementById("txt2").classList.remove("appearMenuRight");
+}
+
+document.getElementById("cont3").addEventListener('mouseout', close3);
+
+function close3() {
+    document.getElementById("cont3").style.transform = 'translateX(-0vh)';
+    document.getElementById("img3").style.transform = 'scale(1, 1)';
+    document.getElementById("img3").style.marginLeft = '0.25vw';
+    document.getElementById("cont3").classList.remove("openMenuRight");
+    document.getElementById("img3").classList.remove("appearMenuRight");
+    document.getElementById("txt3").classList.remove("appearMenuRight");
+}
+
+document.getElementById("cont4").addEventListener('mouseout', close4);
+
+function close4() {
+    document.getElementById("cont4").style.transform = 'translateX(-0vh)';
+    document.getElementById("img4").style.transform = 'scale(1, 1)';
+    document.getElementById("img4").style.marginLeft = '0.25vw';
+    document.getElementById("cont4").classList.remove("openMenuRight");
+    document.getElementById("img4").classList.remove("appearMenuRight");
+    document.getElementById("txt4").classList.remove("appearMenuRight");
+}
+
+document.getElementById("cont5").addEventListener('mouseout', close5);
+
+function close5() {
+    document.getElementById("cont5").style.transform = 'translateX(-0vh)';
+    document.getElementById("img5").style.transform = 'scale(1, 1)';
+    document.getElementById("img5").style.marginLeft = '0.25vw';
+    document.getElementById("cont5").classList.remove("openMenuRight");
+    document.getElementById("img5").classList.remove("appearMenuRight");
+    document.getElementById("txt5").classList.remove("appearMenuRight");
+}
+
+document.getElementById("cont6").addEventListener('mouseout', close6);
+
+function close6() {
+    document.getElementById("cont6").style.transform = 'translateX(-0vh)';
+    document.getElementById("img6").style.transform = 'scale(1, 1)';
+    document.getElementById("img6").style.marginLeft = '0.25vw';
+    document.getElementById("cont6").classList.remove("openMenuRight");
+    document.getElementById("img6").classList.remove("appearMenuRight");
+    document.getElementById("txt6").classList.remove("appearMenuRight");
+}
